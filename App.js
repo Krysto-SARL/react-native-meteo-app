437b3d093a1802dd24754864948c0987158a0a79
Krysto <krysto.contact@gmail.com>
2023-07-15 21:58:02 +1100

    mise en place du router

--- App.js
+++ App.js
@@ -4,7 +4,18 @@ import Home from './pages/Home/Home'
 import { ImageBackground } from 'react-native'
 import BackgroundImg from './assets/background.png'
 import AlataRegular from './assets/fonts/Alata-Regular.ttf'
+import Forecast from './pages/Forecast/Forecast'
 import { useFonts } from 'expo-font'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
+
+const navTheme = {
+  colors: {
+    background: 'transparent',
+  },
+}
 
 export default function App() {
   const [isFontLoaded] = useFonts({
@@ -12,16 +23,26 @@ export default function App() {
   })
 
   return isFontLoaded ? (
-    <ImageBackground
-      source={BackgroundImg}
-      style={s.img_background}
-      imageStyle={s.img}
-    >
-      <SafeAreaProvider>
-        <SafeAreaView style={s.appContainer}>
-          {isFontLoaded ? <Home /> : null}
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </ImageBackground>
+    <NavigationContainer theme={navTheme}>
+      <ImageBackground
+        source={BackgroundImg}
+        style={s.img_background}
+        imageStyle={s.img}
+      >
+        <SafeAreaProvider>
+          <SafeAreaView style={s.appContainer}>
+            {
+              <Stack.Navigator
+                screenOptions={{ headerShown: false }}
+                initialRouteName="Home"
+              >
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Forecast" component={Forecast} />
+              </Stack.Navigator>
+            }
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ImageBackground>
+    </NavigationContainer>
   ) : null
 }
---
+++ pages/Forecast/Forecast.jsx
@@ -0,0 +1,14 @@
+
+import { s } from './Forecast.style'
+
+import MeteoAdvanced from '../../components/MeteoAdvanced/MeteoAdvanced'
+import Txt from '../../components/Txt/Txt'
+
+export default function Forecast() {
+
+  return  (
+    <>
+     <Txt>Forecast</Txt>
+    </>
+  ) 
+}
