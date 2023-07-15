761e410e00dbeff56c68c7f326b862ba409ded24
Krysto <krysto.contact@gmail.com>
2023-07-15 11:01:56 +1100

    initialiaze app

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { s } from './App.style'
+import Home from './pages/Home/Home'
+import { SafeAreaView } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Home />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { s } from './Home.style';
+
+export default function Home() {
+  return <Text>Home</Text>;
+}
